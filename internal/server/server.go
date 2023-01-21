f00725f669a095f91846b6561f8904ae706ed2ca
vavas <vladimir.kryzhanovskyi@gmail.com>
2023-01-21 13:56:49 +0200

    base service structure

---
+++ internal/server/server.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	"flag"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"os"
+	"rrhub/internal/handlers"
+	"time"
+
+	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
+)
+
+var (
+	addr = flag.String("addr", os.Getenv("PORT"), "")
+	cert = flag.String("cert", "", "")
+	key  = flag.String("key", "", "")
+)
+
+func Run() error {
+	flag.Parse()
+	if *addr == ":" {
+		*addr = ":8080"
+	}
+
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
+	app.Use(logger.New())
+	app.Use(cors.New())
+
+	app.Get("/", handlers.Welcome)
+	app.Get("/room/create", handlers.RoomCreate)
+	app.Get("/room/:uuid", handlers.Room)
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWebsocket, websocket.Config{
+		HandshakeTimeout: 10 * time.Second,
+	}))
+	app.Get("/room/:uuid/chat", RoomChat)
+	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
+	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
+	app.Get("/stream/:ssuid/websocket")
+	app.Get("/stream/:ssuid/chat/websocket")
+	app.Get("/stream/:ssuid/viewer/websocket")
+}
